b44ccc9a348267b198e319901652acc32044ba75
KittitutG <kittitut.ganchanapiboon@gmail.com>
2023-12-22 09:02:01 +0700

    cdll-01

--- LinkedList/CircularDoublyLinkedList/CircularDoublyLinkedList.py
+++ LinkedList/CircularDoublyLinkedList/CircularDoublyLinkedList.py
@@ -6,7 +6,7 @@ class Node():
 
     
 class DoublyLinkedList():
-    def    __init__(self):
+    def __init__(self):
         self.head = self.tail = None
 
     def __iter__(self):
@@ -24,6 +24,79 @@ class DoublyLinkedList():
         new_node.next = new_node.prev = new_node
         return "Create successfully"
 
+    def insert(self,value,index):
+        if not self.head:
+            return "Object not exists"
+        else:
+            new_node = Node(value)
+            if index ==0:
+                new_node.next = self.head
+                new_node.prev = self.tail
+                self.head.prev =new_node
+                self.head = new_node
+                self.tail.next = new_node
+            elif index ==1:
+                new_node.next = self.head
+                new_node.prev = self.tail
+                self.head.prev = new_node
+                self.tail.next = new_node
+                self.tail = new_node
+            else:
+                tempNode = self.head
+                ind = 0
+                while ind < index-1:
+                    tempNode = tempNode.next
+                    ind +=1
+                new_node.next =tempNode.next
+                new_node.prev =tempNode
+                new_node.next.prev = new_node
+                tempNode.next = new_node
+            return "Insertion Successfully"
+
+    def traversal(self):
+        if not self.head:
+            return "This is empty"
+        else:
+            tempNode= self.head
+            while tempNode:
+                print(tempNode.value)
+                if tempNode == self.tail:
+                    break
+                tempNode =tempNode.next
+
+    def reverse_travesal(self):
+        if not self.head:
+            return "This is empty"
+        else:
+            tempNode = self.tail
+            while tempNode:
+                print(tempNode.value)
+                if tempNode == self.head:
+                    break
+                tempNode = tempNode.prev
+
+    def search(self,target):
+        if not self.head:
+            return "This is empty"
+        else:
+            tempNode = self.head
+            while tempNode:
+                
+                if tempNode.value == target:
+                    return True
+                if tempNode == self.tail:
+                    break
+                tempNode =tempNode.next
+                
+            return "Element not found"
+
 my_cdll = DoublyLinkedList()
 my_cdll.create(20)
+my_cdll.insert(0,0)
+my_cdll.insert(1,1)
+my_cdll.insert(2,2)
+
 print([i.value for i in my_cdll])
+my_cdll.traversal()
+my_cdll.reverse_travesal()
+print(my_cdll.search(9))
