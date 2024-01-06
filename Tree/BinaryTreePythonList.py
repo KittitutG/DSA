49245b6ddc410ba7ad781474cf8449ce1f65c7e7
KittitutG <kittitut.ganchanapiboon@gmail.com>
2024-01-06 09:24:58 +0700

    bintree-02

--- Tree/BinaryTreePythonList.py
+++ Tree/BinaryTreePythonList.py
@@ -1,3 +1,15 @@
+'''
+create
+insert 
+delete 
+search
+traverse
+    -PreOrder:  root / Left / Right
+    -InOrder:   Left / root / Right
+    -PostOrder: Left / Right / root
+    -LevelOrder: Level wiese
+delete tree
+'''
 class BinaryTree:
     def __init__(self,size) -> None:
         self.customList = size *[None]
@@ -31,7 +43,32 @@ class BinaryTree:
         print(self.customList[index])
         self.InOrderTraversal(index*2 +1)
 
+    def PostOrderTraversal(self,index):
+        if index > self.lastUsedIndex:
+            return
+        self.PostOrderTraversal(index*2)
+        self.PostOrderTraversal(index*2 +1)
+        print(self.customList[index])
 
+    def LevelOrderTraversal(self,index):
+        for i in range(index,self.lastUsedIndex+1):
+            print(self.customList[i])
+
+    def deleteNode(self,value):
+        if self.lastUsedIndex ==0:
+            return "Empty tree"
+        else:
+            for i in range(self.lastUsedIndex):
+                if self.customList[i] == value:
+                    self.customList[i] = self.customList[self.lastUsedIndex]
+                    self.customList[self.lastUsedIndex] = None
+                    self.lastUsedIndex -=1
+                    return "delete Successfully"
+                
+
+    def deleteTree(self):
+        self.customList = None
+        
 newBT = BinaryTree(8)
 
 newBT.insert("Drinks")
@@ -41,4 +78,8 @@ newBT.insert('Tea')
 newBT.insert('Coffee')
 
 # newBT.PreOrderTraversal(1)
-newBT.InOrderTraversal(1)
+# newBT.InOrderTraversal(1)
+# newBT.PostOrderTraversal(1)
+
+# newBT.deleteNode("Tea")
+newBT.LevelOrderTraversal(1)
