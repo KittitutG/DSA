ac7bc01a1c7885a8fe538cffe9054cc7e82d772a
KittitutG <kittitut.ganchanapiboon@gmail.com>
2024-01-24 09:28:50 +0700

    bubble_sort

---
+++ Sorting/01_Bubble_sort.py
@@ -0,0 +1,21 @@
+'''
+repeatedly compare pair of items and swap them if they're in wrong order
+
+time complexity: O(n^2)
+space complexity: O(1)
+
+when to use:
+    -input is sorted 
+    -space is a concern
+    -easy to implement
+when to avoid:
+    -average time complexity is poor
+'''
+
+def bubbleSort(input_list):
+    for i in range(len(input_list)-1):
+        for j in range(len(input_list)-i-1):
+            if input_list[j+1] > input_list[j]:
+                input_list[j],input_list[j+1] = input_list[j+1],input_list[j]
+    print(input_list)
+
